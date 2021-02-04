e7bac2bec6907fb39f6c0dd1a390f582561e1a76
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-04 09:25:58 +0000

    refactored happy path test block

--- spec/spell_checker_spec.rb
+++ spec/spell_checker_spec.rb
@@ -2,16 +2,10 @@ require 'spell_checker'
 
 describe "#spell_checker" do
   describe "happy path" do
-    it "should receive 'a' and return 'a'" do
-      expect(spell_checker('a')).to eq 'a'
-    end
-
-    it "should receive 'a cat' and return 'a cat'" do
-      expect(spell_checker('a cat')).to eq 'a cat'
-    end
-
     it "should receive 'a cat sat' and return 'a cat sat'" do
       expect(spell_checker('a cat sat')).to eq 'a cat sat'
     end
   end
+
+
 end
