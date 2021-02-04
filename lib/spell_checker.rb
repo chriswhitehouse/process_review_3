e5f40237a1598ba36492585ffc7578adb90dc033
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-04 09:38:21 +0000

    refactored incorect spelling block tests

--- lib/spell_checker.rb
+++ lib/spell_checker.rb
@@ -1,11 +1,23 @@
 def spell_checker(string)
 
-  if string == 'a dat'
-    'a ~dat~'
-  elsif string == 'a dat xat'
-    'a ~dat~ ~xat~'
-  else
-    return string
+  correct_spelling_library = [
+    'a',
+    'cat',
+    'sat'
+  ]
+
+  output_array = []
+
+  sentance_array = string.split(' ')
+
+  sentance_array.each do |word|
+    if correct_spelling_library.include?(word)
+      output_array.push(word)
+    else
+      output_array.push("~#{word}~")
+    end
   end
+
+  return output_array.join(' ')
 
 end
